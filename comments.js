b6de573c2a2243fb90d8568b2e927de2f3916765
imshalidrees <imshalidrees@gmail.com>
2025-02-04 14:46:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,33 +1,9 @@
 // Create a web server
-
-// 1. Load the http module
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var path = require('path');
-
-// 2. Create a server
-http.createServer(function (request, response) {
-  console.log('request ', request.url);
-  var pathname = url.parse(request.url).pathname;
-  console.log('pathname ', pathname);
-  var filePath = path.join(__dirname, 'public', pathname);
-  console.log('filePath ', filePath);
-
-  fs.readFile(filePath, 'utf8', function (err, data) {
-    if (err) {
-      response.writeHead(404, {
-        'Content-Type': 'text/html'
-      });
-      response.end('<h1>404 Not Found</h1>');
-    } else {
-      response.writeHead(200, {
-        'Content-Type': 'text/html'
-      });
-      response.end(data);
-    }
-  });
-
-}).listen(3000);
-
-console.log('Server running at http://
+// Create a route for GET /comments
+// Create a route for POST /comments
+// Create a route for GET /comments/:id
+// Create a route for PUT /comments/:id
+// Create a route for DELETE /comments/:id
+// Create a route for POST /comments/:id/vote-up
+// Create a route for POST /comments/:id/vote-down
+// Create a route for POST /comments/:id/vote-reset
